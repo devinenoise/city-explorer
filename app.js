d6bd02011ef358854dc521225c7d82866bd4800b
Kyle Devine <devinenoise@gmail.com>
2020-02-19 21:21:50 -0800

    yelp and eventful added

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-
 const express = require('express');
 const request = require('superagent');
 const app = express();
-// const darkSky = require('./data/darksky.json');
 const port = process.env.PORT || 3000;
 
 
@@ -14,7 +12,7 @@ let lat;
 let lng;
 
 // location route
-app.get('/location', async (req, res, next) => {
+app.get('/location', async (req, res) => {
     try {
 
         const location = req.query.search;
@@ -23,7 +21,7 @@ app.get('/location', async (req, res, next) => {
 
         const firstResult = cityData.body[0];
 
-        // update the state of the lat and long for global scope
+        // update the state of the lat and long
         lat = firstResult.lat;
         lng = firstResult.lon;
 
@@ -34,12 +32,12 @@ app.get('/location', async (req, res, next) => {
                 longitude: lng
             });
     } catch (err) {
-        next(err);
+        res.status(500).send('Sorry something went wrong, please try again');
     }
 });
 
 // weather route
-app.get('/weather', async (req, res, next) => {
+app.get('/weather', async (req, res) => {
     try {
 
         const portlandWeather = await getWeatherData(lat, lng);
@@ -47,9 +45,59 @@ app.get('/weather', async (req, res, next) => {
         res.json(portlandWeather);
 
     } catch (err) {
-        next(err);
+        res.status(500).send('Sorry something went wrong, please try again');
     }
 });
+
+app.get('/reviews', async (req, res) => {
+    try {
+
+        const yelp = await request
+            .get(`https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${lat}&longitude=${lng}`)
+            .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`);
+
+        const yelpStuff = yelp.body.businesses.map(business => {
+            return {
+                name: business.name,
+                image: business.image_url,
+                price: business.price,
+                rating: business.rating,
+                url: business.url,
+            };
+        });
+
+        res.json(yelpStuff);
+
+    } catch (err) {
+        res.status(500).send('Sorry something went wrong, please try again');
+    }
+});
+
+app.get('/events', async (req, res) => {
+    try {
+        // lat = 32.746682;
+        // lng = -117.162741;
+
+        const eventful = await request
+            .get(`http://api.eventful.com/json/events/search?app_key=${process.env.EVENTBRITE_API_KEY}&where=${lat},${lng}&within=25`);
+
+        const body = JSON.parse(eventful.text);
+        const eventStuff = body.events.event.map(event => {
+            return {
+                link: event.url,
+                name: event.title,
+                date: event.start_time,
+                summary: event.description,
+            };
+        });
+
+        res.json(eventStuff);
+
+    } catch (err) {
+        res.status(500).send('Sorry something went wrong, please try again');
+    }
+});
+
 
 
 // function to map through the weather data based on latitude and longitude coordinates
